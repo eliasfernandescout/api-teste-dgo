2be3dba5d183730b493e080328d06b6faf6c0b3c
eliasfernandescout <eliasfernandescout@gmail.com>
2024-03-03 18:54:15 -0300

    feat: create userSchema

---
+++ src/auth/schemas/user.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({
+  timestamps: true,
+})
+export class User {
+  @Prop()
+  name: string;
+  @Prop()
+  email: string;
+  @Prop()
+  senha: string;
+  @Prop()
+  telefone: string;
+}
+export const UserSchema = SchemaFactory.createForClass(User);
